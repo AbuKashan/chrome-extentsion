215b4e91101c2b6fbffc5484117a4a6fa9d8b539
AbuKashan <75110976+AbuKashan@users.noreply.github.com>
2022-02-23 09:39:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+
+let myLeads=[]
+const inputEl=document.getElementById('input-el')
+const inputBtn=document.getElementById("input-btn")
+const ulEl=document.getElementById("ul-el")
+const dltBtn=document.getElementById("dlt-btn")
+const tabBtn=document.getElementById("save-btn")
+
+const leadsfromlocalstorage=JSON.parse(localStorage.getItem("myLeads") )
+
+if(leadsfromlocalstorage){
+  myLeads=leadsfromlocalstorage
+  render(myLeads)
+}
+
+
+
+tabBtn.addEventListener("click",function(){
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    myLeads.push(tabs[0].url)
+     localStorage.setItem("myLeads",JSON.stringify(myLeads) )
+     render(myLeads)
+})
+})
+
+
+dltBtn.addEventListener("dblclick",function(){
+     localStorage.clear()
+     myLeads=[]
+     render(myLeads)
+})
+
+
+inputBtn.addEventListener("click",function(){
+   myLeads.push(inputEl.value)
+   inputEl.value=""
+   localStorage.setItem("myLeads",JSON.stringify(myLeads))
+   render(myLeads)
+})
+
+function render(leads){
+  let listItems=""
+  for(let i=0;i<leads.length;i++){
+    listItems+="<li><a target='_blank' href='" +leads[i] + "'>"+leads[i]+"</a></li>"
+  }
+   ulEl.innerHTML=listItems
+}
